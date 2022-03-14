299dbb53934e4b75e3688d5091ee00b502f6736d
Nikitamar95 <93392324+Nikitamar95@users.noreply.github.com>
2022-03-14 11:40:17 +0300

    Add files via upload

---
+++ Linux/Service_arrays.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+#  Service_arrays.py
+#
+#  Copyright 2022 Nikita Marchenko
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+# Служебные массивы
+exit_array = ["в", "В", "B"]
+about_array = ["о", "О", "o", "O"]
+monografy_array = ["м", "М", "M"]
+journal_array = ["ж", "ж", "Ж"]
+compilation_array = ["с", "C", "c", "C"]
+license_array = ["л", "Л"]
+yes_arr = ["д", "Д"]
+no_arr = ["н", "Н", "H"]
+batch_arr = ["п", "П"]
+cleargy_arr = [
+    "чт.",
+    "иподиак.",
+    "иеродиак.",
+    "иеромон.",
+    "архим.",
+    "архимандр.",
+    "митр.",
+    "патр.",
+    "мон."
+    ]
+cleargy_dict = {
+    "диак.": "deacon.",
+    "свящ.": "priest.",
+    "прот.": "archpriest.",
+    "еп.": "bishop.",
+    "архиеп.": "archbishop."
+    }
